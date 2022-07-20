95f37eac898fa77262269b1ace8368b0502fb034
Rajarshi Banerjee <rajarshi@pitangent.com>
2022-07-20 23:29:14 +0530

    change in seeder file

--- database/seeders/ConnectionsInCommonSeeder.php
+++ database/seeders/ConnectionsInCommonSeeder.php
@@ -14,16 +14,31 @@ class ConnectionsInCommonSeeder extends Seeder
      *
      * @return void
      */
+
+    
     public function run()
     {
         $users= User::all();
-        for($i=0;$i<50;$i++){
-            for($j=60;$j<79;$j++){
-                Connections::create([
-                    "sender_id" =>$users[$i]->id,
-                    "status"=>2,
-                    "receiver_id"=>$users[$j]->id
-                ]);
+        for($i=0;$i<30;$i++){
+            for($j=60;$j<90;$j++){
+                if($i!=$j){
+                    Connections::create([
+                        "sender_id" =>$users[$i]->id,
+                        "status"=>2,
+                        "receiver_id"=>$users[$j]->id
+                    ]);
+                }  
+            }
+        }
+        for($i=60;$i<70;$i++){
+            for($j=70;$j<90;$j++){
+                if($i!=$j){
+                    Connections::create([
+                        "sender_id" =>$users[$i]->id,
+                        "status"=>2,
+                        "receiver_id"=>$users[$j]->id
+                    ]);
+                }  
             }
         }
     }
